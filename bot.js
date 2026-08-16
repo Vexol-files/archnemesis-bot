faecbcf1a59f992f2e6a04164b1007bae88e2b8b
Vexol-files <wolskidawid07@gmail.com>
2026-08-16 12:55:00 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,17 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once("ready", () => {
+  console.log(`Bot online jako ${client.user.tag}`);
+});
+
+// Bot-hosting.net automatycznie wstawia BOT_TOKEN do ENV
+client.login(process.env.BOT_TOKEN);
